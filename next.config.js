fdd3879f0da9313ffe1b5912dc530bdd6cce074e
heintzz <mhasnanr@gmail.com>
2023-05-27 10:03:52 +0700

    feat: add api endpoint for activity

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    ACTIVITY_ENDPOINT: process.env.ACTIVITY_ENDPOINT,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
