c4970e2ac0eba674b3e19d0f585817352f7df22c
heintzz <mhasnanr@gmail.com>
2023-05-28 14:57:49 +0700

    feat: add colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        cancelGray: '#4A4A4A',
+        cancelWhite: '#F4F4F4',
+        delete: '#ED4C5C',
+        primary: '#16ABF8',
+      },
+    },
+  },
 };
